4cf3069d7f49a020215f04428a5d5badcbb7ef35
chrysacier <chrysantheme98@gmail.com>
2021-05-25 17:48:11 +0200

    salut

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -22,7 +22,7 @@ const json = fetch('assets/js/data.json').then(function(response) {
     view_all_data();
 });
 
-function view_all_data() {
+function view_all_data(){
     if(exp_planet){
         if(localStorage.getItem("data-planet")){
             let stored_planet = localStorage.getItem("data-planet");
@@ -61,22 +61,8 @@ buttons_exp.forEach(button => {
         e.preventDefault();
         let btn_data = button.getAttribute("data-type");
         let clicked_planet = exp_planet.getAttribute('data-planet');
-        let measure = " ";
-        if (btn_data == "Distance") {
-            measure = "million km";
-        } else if (btn_data == "Diameter") {
-            measure = "km";
-        } else if (btn_data == "Mass") {
-            measure = "x10<sup>24</sup> kg";
-        } else if (btn_data == "Rotation_Period") {
-            measure = "hours";
-        } else if (btn_data == "Gravity") {
-            measure = "m/s²";
-        } else if (btn_data == "Mean_Temperature") {
-            measure = "°C";
-        } else if (btn_data == "Number_of_satellites") {
-            measure = "satellites";
-        }
+        let measure = getMeasure(btn_data);
+
         let planet_filter = planetdata.filter((planets) => {
             return planets.Name.toLowerCase().includes(clicked_planet.toLocaleLowerCase());
         });
@@ -112,22 +98,7 @@ buttons_all.forEach(button => {
             planet_data_mono.classList.remove("hidden");
         });
         let btn_data = button.getAttribute("data-type");
-        let measure = " ";
-        if (btn_data == "Distance") {
-            measure = "million km";
-        } else if (btn_data == "Diameter") {
-            measure = "km";
-        } else if (btn_data == "Mass") {
-            measure = "x10<sup>24</sup> kg";
-        } else if (btn_data == "Rotation_Period") {
-            measure = "hours";
-        } else if (btn_data == "Gravity") {
-            measure = "m/s²";
-        } else if (btn_data == "Mean_Temperature") {
-            measure = "°C";
-        } else if (btn_data == "Number_of_satellites") {
-            measure = "satellites";
-        }
+        let measure = getMeasure(btn_data);
         planet_all_item.forEach(planet_one_item => {
             let solo_planet_data = planet_one_item.getAttribute("data-planet");
             let planet_filter = planetdata.filter((planets) => {
@@ -175,7 +146,6 @@ buttons_filter.forEach(button_filter => {
         planet_all_item.forEach(planet_item => {
             let planet_item_data = planet_item.getAttribute("data-planet");
             if (filter_index > 0) {
-                console.log("1");
                 if (button_filter_data == planet_item_data) {
                     if (button_filter.classList.contains("btn--filter-active")) {
                         if (button_filter_data == "Neptune" && filter_index == 1) {
@@ -201,7 +171,6 @@ buttons_filter.forEach(button_filter => {
                     }
                 }
             } else if (filter_index == 0 && previous_filter_index > filter_index) {
-                console.log("2");
                 check = true;
                 buttons_filter.forEach(button => {
                     button.classList.add("btn--filter-active");
@@ -214,7 +183,6 @@ buttons_filter.forEach(button_filter => {
                     check = false;
                 }
             } else if (filter_index == 0 && check == false) {
-                console.log("3");
                 previous_filter_index = 0;
                 if (planet_item_data == "Neptune") {
                     previous_filter_index = filter_index;
@@ -237,7 +205,13 @@ planet_all_item.forEach(planet_item => {
         let planet_item_data = planet_item.getAttribute("data-planet");
         localStorage.setItem("data-planet", planet_item_data);
     });
-   
+});
+let block_planets__item = document.querySelectorAll(".block-planets__item")
+block_planets__item.forEach(block_planet__item => {
+    block_planet__item.addEventListener('click', (e) =>{
+        let block_planet__item_data = block_planet__item.getAttribute("data-planet");
+        localStorage.setItem("data-planet", block_planet__item_data);
+    });
 });
 
 var menu = ['M', 'V', 'E', 'M', 'J', 'S', 'U', 'N'];
@@ -245,7 +219,7 @@ let swiper_container = document.querySelector(".swiper-container");
 if (swiper_container) {
     
     var mySwiper = new Swiper('.swiper-container', {
-        
+
         breakpoints: {
             1280: {
                 direction: 'vertical',
@@ -271,6 +245,8 @@ if (swiper_container) {
         
     }
     mySwiper.on('activeIndexChange', function() {
+        let swiper_slide_prev = document.querySelector(".swiper-slide-prev");
+        let swiper_slide_next = document.querySelector(".swiper-slide-next");
         let slide_active = document.querySelector(".swiper-slide-active");
         let slide_active_data = slide_active.getAttribute("data-swiper-slide-index");
         if (mySwiper.activeIndex == "8" | mySwiper.activeIndex == "16") {
@@ -299,22 +275,8 @@ if (swiper_container) {
                 planet_title.innerHTML = `${planets.Name}`;
                 planet_infos.innerHTML = `${planets.Informations}`;
             } else {
-                let measure = " ";
-                if (previous_data == "Distance") {
-                    measure = "million km";
-                } else if (previous_data == "Diameter") {
-                    measure = "km";
-                } else if (previous_data == "Mass") {
-                    measure = "x10<sup>24</sup> kg";
-                } else if (previous_data == "Rotation_Period") {
-                    measure = "hours";
-                } else if (previous_data == "Gravity") {
-                    measure = "m/s²";
-                } else if (previous_data == "Mean_Temperature") {
-                    measure = "°C";
-                } else if (previous_data == "Number_of_satellites") {
-                    measure = "satellites";
-                }
+                let measure = getMeasure(previous_data);
+
                 planet_title.innerHTML = `${planets[previous_data]} <span class="planet-data--unit">${measure}</span>`;
                 planet_infos.innerHTML = `${planets['Facts'][0][previous_data]}`
             }
@@ -329,6 +291,8 @@ if (swiper_container) {
             mySwiper.mousewheel.disable();
         }
     }
+    
+
     reportWindowSize();
 }
 new kursor({
@@ -388,10 +352,65 @@ if (el){
     function translate(e) {
         const delta = e.deltaY;
         const currentScroll = el.scrollLeft;
-        console.log(el)
         el.scrollTo(currentScroll + delta, 0)
-        e.preventDefault();
+        
+        if (delta > 0) {
+            e.preventDefault();
+        }
     }
 }
 
 
+let text_cursor = document.querySelector(".text-cursor")
+const onMouseMove = (e) =>{
+    if(exp_planet){
+        console.log("1")
+        text_cursor.style.left = e.pageX+10 + 'px';
+        text_cursor.style.top = e.pageY+10 + 'px';
+    
+    }else if(document.querySelector(".planet-all")){
+        console.log("1")
+        console.log(planet_all_item)
+
+        text_cursor.style.left = e.pageX-120 + 'px';
+        text_cursor.style.top = e.pageY + 'px';
+    }
+}
+document.addEventListener('mousemove', onMouseMove);
+
+let oldscroll = 0
+let logo_letters = document.querySelectorAll(".hide-scroll")
+window.addEventListener("scroll", (e) =>{
+
+   if (oldscroll < window.scrollY){
+        logo_letters.forEach(logo_letter => {
+            logo_letter.classList.add("hide-scroll--hidden");
+        });
+   }else{
+        logo_letters.forEach(logo_letter => {
+            logo_letter.classList.remove("hide-scroll--hidden");
+        });
+   };
+   oldscroll = window.scrollY;
+});
+
+
+function getMeasure(btn_data){
+    let measure = " ";
+    if (btn_data == "Distance") {
+        measure = "million km";
+    } else if (btn_data == "Diameter") {
+        measure = "km";
+    } else if (btn_data == "Mass") {
+        measure = "x10<sup>24</sup> kg";
+    } else if (btn_data == "Rotation_Period") {
+        measure = "hours";
+    } else if (btn_data == "Gravity") {
+        measure = "m/s²";
+    } else if (btn_data == "Mean_Temperature") {
+        measure = "°C";
+    } else if (btn_data == "Number_of_satellites") {
+        measure = "satellites";
+    }
+    return measure;
+}
